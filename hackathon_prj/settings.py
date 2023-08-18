dff635abb8556f7fb4e0161f7bd9d1f8d91a5dc6
lia715 <sy0715p@naver.com>
2023-08-18 22:23:32 +0900

    230818 settings

--- hackathon_prj/settings.py
+++ hackathon_prj/settings.py
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "Ko-kr"
+LANGUAGE_CODE = "Ko"
 
 TIME_ZONE = "Asia/Seoul"
 
