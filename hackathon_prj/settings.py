decb632e54fa42913acbe589c1f63794e82b2d4a
lia715 <sy0715p@naver.com>
2023-08-18 22:32:35 +0900

    230818 Allow

--- hackathon_prj/settings.py
+++ hackathon_prj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-a!xp2w8@6b$ztoa8w$s737%8a+o1pt$j5*lknc@=t1@+=&7s_s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
